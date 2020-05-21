874466acf671cd1f50456a489f1e58144cf63326
Martin Regec <martin.regec310@gmail.com>
2020-05-21 13:46:14 +0200

    added enums for color and direction

---
+++ SnakeGame/enums.js
@@ -0,0 +1,16 @@
+
+const Enums =
+    {
+        WHITE: 0,
+        BLACK: 1,
+        RED: 2,
+        GREEN: 3
+    }
+
+const Direction =
+    {
+        UP: 0,
+        DOWN: 1,
+        LEFT: 2,
+        RIGHT: 3
+    }
