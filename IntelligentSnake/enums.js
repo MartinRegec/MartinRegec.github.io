68324d2058fc344eaf3ff7109b4ab89cba99d7cf
Martin Regec <martin.regec310@gmail.com>
2020-05-21 15:50:19 +0200

    minor update

--- IntelligentSnake/enums.js
+++ IntelligentSnake/enums.js
@@ -5,12 +5,23 @@ const Color =
         BLACK: 1,
         RED: 2,
         GREEN: 3
-    }
+    };
 
 const Direction =
     {
         UP: 0,
         DOWN: 1,
         LEFT: 2,
-        RIGHT: 3
-    }
+        RIGHT: 3,
+        get_opposite(direction)
+        {
+            if (direction === Direction.RIGHT)
+                return Direction.LEFT
+            else if (direction === Direction.LEFT)
+                return Direction.RIGHT
+            else if (direction === Direction.DOWN)
+                return Direction.UP
+            else if (direction === Direction.UP)
+                return Direction.DOWN
+        }
+    };
