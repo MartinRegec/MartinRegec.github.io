f1053ca339a0104f1b97af2bc6be4f291e9755ad
Martin Regec <martin.regec310@gmail.com>
2020-05-21 14:00:14 +0200

    added implementation for class Point

---
+++ SnakeGame/point.js
@@ -0,0 +1,44 @@
+import Color from 'SnakeGame/enums.js'
+
+class Point
+{
+    constructor(x, y, color)
+    {
+        this.x_ = x;
+        this.y_ = y;
+        this.color_ = color;
+    }
+
+    get color()
+    {
+        return this.color_
+    }
+
+    get x()
+    {
+        return this.x_
+    }
+
+    get y()
+    {
+        return this.y_
+    }
+
+    set color(color)
+    {
+        this.color_ = color
+    }
+
+    copy()
+    {
+        return new Point(this.x, this.y, this.color)
+    }
+
+    static compare(point1, point2)
+    {
+        if ((point1.x == point2.x) && (point1.y == point2.y))
+            return true;
+        else
+            return false;
+    }
+}
